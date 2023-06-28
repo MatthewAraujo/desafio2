4a6893bce196b15ec0179a38953a64229e831ef1
Matthew Araujo <matthewaraujo20@gmail.com>
2023-06-28 09:52:35 -0300

    subindo

--- desafio2.js
+++ desafio2.js
@@ -14,6 +14,8 @@ class paciente{
     this.nome = nome;
     this.data = data;
 
+
+
     this.pacientes.push(this.cpf,this.nome,this.data);
     
     console.log("Paciente adicionado com sucesso!");
@@ -22,6 +24,61 @@ class paciente{
   removePaciente(cpf){
     const index = this.pacientes.find()
   }
+
+
+
+  formatarCpf(cpf){
+    return cpf.replace(/(\d{3})(\d{3})(\d{3})(\d{2})/, "$1.$2.$3-$4");
+  }
+
+  adicionarData(data){
+    var dataNascimentoParts = data.split("/");
+    var dia = parseInt(dataNascimentoParts[0]);
+    var mes = parseInt(dataNascimentoParts[1]) - 1; 
+    var ano = parseInt(dataNascimentoParts[2]);
+    var dataNascimentoObj = new Date(ano, mes, dia);
+    var dataAtual = new Date();
+
+
+    if (isNaN(dataNascimentoObj) || dataNascimentoObj >= dataAtual) {
+      console.log("Erro: A data de nascimento deve estar no formato DD/MM/AAAA e ser anterior à data atual.");
+      validarCliente()
+      return;
+    }
+  
+    var idade = calcularIdade(dataNascimentoObj, dataAtual);
+    if (idade < 13) {
+      console.log("Erro: O cliente deve ter um acompanhante.");
+      validarCliente()
+      return;
+    }
+  
+
+  }
+
+  formatarData(data){
+    var dia = adicionarZeroEsquerda(data.getDate());
+    var mes = adicionarZeroEsquerda(data.getMonth() + 1);
+    var ano = data.getFullYear();
+    return dia + "/" + mes + "/" + ano;
+  }
+
+  adicionarZeroEsquerda(valor) {
+    return valor < 10 ? "0" + valor : valor;
+  }  
+
+  calcularIdade(dataNascimento, dataAtual) {
+    var diffAnos = dataAtual.getFullYear() - dataNascimento.getFullYear();
+    var diffMeses = dataAtual.getMonth() - dataNascimento.getMonth();
+    var diffDias = dataAtual.getDate() - dataNascimento.getDate();
+  
+    if (diffMeses < 0 || (diffMeses === 0 && diffDias < 0)) {
+      diffAnos--;
+    }
+  
+    return diffAnos;
+  }
+
 }
 
 let opcao = 0;
